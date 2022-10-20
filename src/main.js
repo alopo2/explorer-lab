9a764fa61c18829eb88288df15eaa5c586504c6a
Angela Carvalho <angelalopo@outlook.com>
2022-10-20 12:57:34 -0300

    cria mascara numero cartao + regEx

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ const securityCodeMasked = IMask(securityCode, securityCodePattern)
 const expirationDate = document.querySelector("#expiration-date")
 
 const expirationDatePattern = {
-  mask: "MM{/}00",
+  mask: "MM{/}YY",
   blocks: {
     YY: {
       mask: IMask.MaskedRange,
@@ -45,3 +45,26 @@ const expirationDatePattern = {
 }
 
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,12})/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "dafault",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+  },
+}
